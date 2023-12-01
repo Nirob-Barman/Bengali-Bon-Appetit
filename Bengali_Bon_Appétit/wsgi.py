f672ebbaea99bf2e19e3cf45287f4a2baf11f6f8
Nirob-Barman <niroborin111@gmail.com>
2023-12-01 08:14:29 +0600

    project & two app

---
+++ Bengali_Bon_Appétit/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Bengali_Bon_Appétit project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Bengali_Bon_Appétit.settings')
+
+application = get_wsgi_application()
